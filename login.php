f3793ea448037108ef3800778b69f7f76ac3668e
Asif Rahman <nytmare.717@gmail.com>
2022-08-30 08:01:36 +0600

    dummy file for php testing

--- login.php
+++ login.php
@@ -12,23 +12,6 @@
 
 <body>
 
-    <form method="post" action="" name="signup-form">
-        <div class="form-element">
-            <label>Username</label>
-            <input type="text" name="username" pattern="[a-zA-Z0-9]+" required />
-        </div>
-        <div class="form-element">
-            <label>Email</label>
-            <input type="email" name="email" required />
-        </div>
-        <div class="form-element">
-            <label>Password</label>
-            <input type="password" name="password" required />
-        </div>
-        <button type="submit" name="register" value="register">Register</button>
-    </form>
-
-    </form>
 
 </body>
 
